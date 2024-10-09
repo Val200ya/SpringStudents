bdb61b7d539d561662804b86ec83836d356d503e
Val200ya <australia636@gmail.com>
2024-10-09 14:35:22 +0300

    springstudents

---
+++ src/main/java/com/example/springstudents/model/Student.java
@@ -0,0 +1,16 @@
+package com.example.springstudents.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Student {
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String email;
+    private int age;
+
+}
---
+++ src/main/java/com/example/springstudents/model/University.java
@@ -0,0 +1,5 @@
+package com.example.springstudents.model;
+
+public class University {
+
+}
